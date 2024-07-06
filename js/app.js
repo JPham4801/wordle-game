4166913381bfb1fb72b52e8ed318a10f5c3629c6
JPham4801 <JPham4801@gmail.com>
2024-07-06 00:57:50 -0500

    added invalid prompts for not enough letters/invalid word

--- js/app.js
+++ js/app.js
@@ -4,31 +4,29 @@
 //// As a user, I want a keyboard layout so that I can choose to click or type in my selection.
 //// As a user, I want to hit enter or click the enter button so that I can validate my word.
 //// As a user, I want a visual representation of how many chances I have left.
-// As a user, I want my tiles to perform an animation when I submit my word choice.
+//// As a user, I want my tiles to perform an animation when I submit my word choice.
 //// As a user, I want the inputs tiles to be highlighted green, yellow, or grey so that I know which inputs are in the the correct place, wrong place, or not used at all respectively.
-// As a user, I want inputs that I have previously used to be null and cannot be reused.
+//// As a user, I want inputs that I have previously used to be null and cannot be reused.
 // As a user, I want word validation so that I cannot use a word not on the list.
-// As a user, I want the game to end when I entered the correct answer.
-// As a user, I want a word reveal when I have not guessed the correct word in the set amount of tries.
-// As a user, I want the option to play again after I complete my game.
-// As a user, I want the option to play again at any given time to start over with a new word
+//// As a user, I want the game to end when I entered the correct answer.
+//// As a user, I want a word reveal when I have not guessed the correct word in the set amount of tries.
+//// As a user, I want the option to play again after I complete my game.
+//// As a user, I want the option to play again at any given time to start over with a new word
+
+//todo
+//// dynamically make tiles once random word is chose to equal the word length
+//// add different arrays
+//// change color of on-screen keyboard according to letter color
+//// add animations
+//// modal
+////  - diff arrays button options
+////  - button in modal to select
+//// change modal cached element ref names
 
 //! ---------- STRETCH GOALS ----------
 // refactor the '.toUpperCase' throughout code (possibly not needed after implmenting arrays)
 // (Yellow tiles) if letter is in the right place but does not exist anywhere else, make those tiles false (maybe forEach method)
 // refactor css (especially the modal section)
-
-//todo
-// dynamically make tiles once random word is chose to equal the word length
-// add different arrays
-// change color of on-screen keyboard according to letter color
-// add animations
-// modal
-//  - diff arrays button options
-//  - button in modal to select
-// change modal cached element ref names
-
-//!reminder   Wordle = words, wordList    colors = colors, colors
 
 /*-------------------------------- Constants --------------------------------*/
 
@@ -45,7 +43,10 @@ const inputEl = document.querySelectorAll('.input-tile');
 const board = document.querySelector('.board-area');
 const gameoverMessage = document.getElementById('gameover-message');
 const categoryMessage = document.getElementById('category-message');
-const keyboardBtn = document.querySelectorAll('.keyboard-btn')
+const keyboardBtn = document.querySelectorAll('.keyboard-btn');
+const invalidPromptContainer = document.querySelector(
+  '.invalid-prompt-container'
+);
 // modal element references
 const howToBtn = document.getElementById('info-button');
 const categoriesBtn = document.getElementById('select-category-btn');
@@ -58,11 +59,11 @@ const titleEl = document.querySelector('.header'); //! remove in final product
 
 /*-------------------------------- Functions --------------------------------*/
 const init = () => {
-  categoriesModal.style.display = 'block';
+  categoriesModal.style.display = 'none';
 };
 
 const generateTiles = () => {
-  // clears default board to dynamically generate new tiles for chosen word
+  // clears default board and dynamically generate new tiles for chosen word
   board.innerHTML = '';
 
   // for loop to generate 6 rows (tries in game) with n numbers of tiles equal to the letters in winning word
@@ -95,9 +96,9 @@ const render = (array, validWordList) => {
   winningWord = array[randomNum].toUpperCase().split(''); // splits word into letters into an array
   categoryMessage.innerText = 'Select a Category';
   gameoverMessage.innerText = '';
-  keyboardBtn.forEach(btn =>{
-    btn.style.backgroundColor = '#757575' // resets keyboard back to default
-  })
+  keyboardBtn.forEach((btn) => {
+    btn.style.backgroundColor = '#757575'; // resets keyboard back to default
+  });
 
   generateTiles();
   howToModal.style.display = 'block';
@@ -168,57 +169,90 @@ const updateTiles = (input, idx) => {
 const isValidWord = () => {
   if (playerWord.length !== winningWord.length) {
     // prevents submission if word isn't filled
+    let prompt = 'Not enough letters!';
+    showInvalidPrompt(prompt);
     return;
   } else if (!validWords.includes(playerWord.join(''))) {
+    let prompt = 'Not a valid word!';
+    showInvalidPrompt(prompt);
     return;
   } else if (validWords.includes(playerWord.join(''))) {
     eval(playerWord, winningWord);
   }
 };
 
-const eval = (playerLetter, correctLetter) => {
-  let numCorrect = 0;
+const showInvalidPrompt = (prompt) => {
+  const invalidPrompt = document.createElement('div');
+  invalidPrompt.className = 'invalid-prompt';
+  invalidPrompt.innerText = prompt;
+  invalidPromptContainer.appendChild(invalidPrompt);
 
-  playerLetter.forEach((element, index) => {
+  setTimeout(() => {
+    invalidPrompt.style.opacity = '0';
+
     setTimeout(() => {
+      invalidPromptContainer.removeChild(invalidPrompt);
+    }, 100);
+  }, 600);
+};
 
-      if (element === correctLetter[index]) {
+const eval = (guessedLetters, targetWordLetters) => {
+  let correctLetterCount = 0;
+
+  guessedLetters.forEach((letter, index) => {
+    setTimeout(() => {
+      if (letter === targetWordLetters[index]) {
         // (Green tile) letter exists and is in the right place
-        let tileColor = '#498047';
-        let tile = document.getElementById(`row-${round}-tile-${index}`);
+        let tileBackgroundColor = '#498047';
+        let tileEl = document.getElementById(`row-${round}-tile-${index}`);
 
-        updateTile(tile, tileColor, playerLetter, index);
-        numCorrect = numCorrect + 1;
+        updateTileAppearance(
+          tileEl,
+          tileBackgroundColor,
+          guessedLetters,
+          index
+        );
+        correctLetterCount = correctLetterCount + 1;
       } else if (
-        correctLetter.includes(element) &&
-        element !== correctLetter[index]
+        targetWordLetters.includes(letter) &&
+        letter !== targetWordLetters[index]
       ) {
         // (Yellow tile) letter exists and is in the wrong place
-        let tileColor = '#84732A';
-        let tile = document.getElementById(`row-${round}-tile-${index}`);
+        let tileBackgroundColor = '#84732A';
+        let tileEl = document.getElementById(`row-${round}-tile-${index}`);
 
-        updateTile(tile, tileColor, playerLetter, index);
-      } else if (!correctLetter.includes(element)) {
+        updateTileAppearance(
+          tileEl,
+          tileBackgroundColor,
+          guessedLetters,
+          index
+        );
+      } else if (!targetWordLetters.includes(letter)) {
         // (Gray tile) letter does not exist anywhere
-        let tileColor = '#3d3d3d';
-        let tile = document.getElementById(`row-${round}-tile-${index}`);
+        let tileBackgroundColor = '#3d3d3d';
+        let tileEl = document.getElementById(`row-${round}-tile-${index}`);
 
-        updateTile(tile, tileColor, playerLetter, index);
+        updateTileAppearance(
+          tileEl,
+          tileBackgroundColor,
+          guessedLetters,
+          index
+        );
       }
     }, index * 200); // delay each iteration by 200ms
   });
 
   setTimeout(() => {
-    console.log(`round ${round} correct ${numCorrect}`);
-    checkForWinner(numCorrect);
+    console.log(`round ${round} correct ${correctLetterCount}`);
+    checkForWinner(correctLetterCount);
 
     round = round + 1;
     playerWord = [];
     console.log(`--Test for round ${round}--`);
-  }, playerLetter.length * 200); // needs delay based on length of player letter, otherwise would colorize next row instead
+  }, guessedLetters.length * 200); // needs delay based on length of player letter, otherwise it would colorize next row instead
 };
 
-const updateTile = (tile, tileColor, playerLetter, index) => {
+const updateTileAppearance = (tile, tileColor, playerLetter, index) => {
   setTimeout(() => {
     tile.style.backgroundColor = tileColor;
   }, 300);
@@ -235,12 +269,14 @@ const checkForWinner = (event) => {
     if (event === winningWord.length && round < 7) {
       gameIsOver = true;
       gameoverMessage.innerText = 'CONGRATULATIONS!';
-      categoryMessage.innerText = "Play again? Select a category."
+      categoryMessage.innerText = 'Play again? Select a category.';
       categoriesModal.style.display = 'block';
     } else if (gameIsOver === false && round === 6) {
       gameIsOver = true;
-      gameoverMessage.innerText = "Oh no you lost!";
-      categoryMessage.innerText = "Play again? Select a category."
+      gameoverMessage.innerText = `You lost! The word was ${winningWord.join(
+        ''
+      )}`;
+      categoryMessage.innerText = 'Play again? Select a category.';
       categoriesModal.style.display = 'block';
     }
   }, winningWord.length * 200);
